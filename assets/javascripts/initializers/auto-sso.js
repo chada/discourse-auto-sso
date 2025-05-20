435382110922701a4fdfcd69c9242debddd7e76f
Charles Chiu <i@chada.me>
2025-05-20 03:35:21 +0000

    fix: loop window reload after sso verified

--- assets/javascripts/initializers/auto-sso.js
+++ assets/javascripts/initializers/auto-sso.js
@@ -7,7 +7,6 @@ export default {
     withPluginApi("0.1", (api) => {
       const siteSettings = container.lookup("service:site-settings");
       const autoSsoEnabled = siteSettings.auto_sso_enabled;
-      const appEvents = container.lookup("service:app-events");
 
       if (!autoSsoEnabled) {
         return;
@@ -23,53 +22,40 @@ export default {
       });
 
       function checkExternalAuth() {
-        // 显示加载状态
-        appEvents.trigger("auto-sso:checking");
-
         ajax(siteSettings.auto_sso_verify_session_url, {
           method: "GET",
           headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
           },
-          withCredentials: true,
           xhrFields: {
             withCredentials: true,
           },
-        })
-          .then((result) => {
-            if (result.authenticated) {
-              // 让用户自动登录
-              const user = result.user;
-              const ssoUrl = "/session/sso";
-              const payload = {
-                email: user.email,
-                name: user.name,
-                username: user.email.split("@")[0], // 使用邮箱前缀作为用户名
-                external_id: user.id,
-                avatar_url: user.image,
-                require_activation: false,
-              };
+        }).then((result) => {
+          if (result.authenticated) {
+            // 让用户自动登录
+            const user = result.user;
+            const ssoUrl = "/session/sso";
+            const payload = {
+              email: user.email,
+              name: user.name,
+              username: user.email.split("@")[0], // 使用邮箱前缀作为用户名
+              external_id: user.id,
+              avatar_url: user.image,
+              require_activation: false,
+            };
 
-              // 构建 SSO 登录 URL
-              const queryString = Object.keys(payload)
-                .map(
-                  (key) =>
-                    `${encodeURIComponent(key)}=${encodeURIComponent(payload[key])}`
-                )
-                .join("&");
+            // 构建 SSO 登录 URL
+            const queryString = Object.keys(payload)
+              .map(
+                (key) =>
+                  `${encodeURIComponent(key)}=${encodeURIComponent(payload[key])}`
+              )
+              .join("&");
 
-              window.location.href = `${ssoUrl}?${queryString}`;
-            }
-          })
-          // should not popup error as it should be quiet
-          // .catch((error) => {
-          //   appEvents.trigger("auto-sso:error", error);
-          //   popupAjaxError(error);
-          // })
-          .finally(() => {
-            appEvents.trigger("auto-sso:checked");
-          });
+            window.location.href = `${ssoUrl}?${queryString}`;
+          }
+        });
       }
     });
   },
